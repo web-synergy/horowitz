78794a289e8d0df05c7782c789ad410c8821139b
Anna Prutnik <prutnik.anna@gmail.com>
2024-01-15 15:14:17 +0200

    feature: add navigation to not found

--- site/src/components/Competition/CompetitionPage.tsx
+++ site/src/components/Competition/CompetitionPage.tsx
@@ -1,7 +1,30 @@
+import { useEffect } from 'react';
+import { useLocation, Navigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useSettingsStore } from '@/store';
 import PageTemplate from '../Common/PageTemplate';
 import { Container } from '@mui/material';
 
 const CompetitionPage = () => {
+  const { pathname } = useLocation();
+
+  const { competitions } = useSettingsStore();
+  const {
+    i18n: { language },
+  } = useTranslation();
+  // if ()
+
+  const langCompetitions = competitions[language];
+  const competitionName = pathname.slice(1);
+
+  const isCompetitionExist = langCompetitions?.find(
+    (item) => item.slug === competitionName
+  );
+
+  if (!isCompetitionExist) {
+    return <Navigate to={'404'} />;
+  }
+
   return (
     <PageTemplate>
       <Container>
