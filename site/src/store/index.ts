3d51baccc081b2291efd9d25a31ebaa72a1f0122
Andrii <andriy30534>
2024-01-16 12:15:09 +0200

    feat: added  getPreviewSettings

--- site/src/store/index.ts
+++ site/src/store/index.ts
@@ -1,28 +1,35 @@
-import { getSettings } from '@/api'
-import { SettingsStoreState } from '@/types/storeTypes'
-import { create } from 'zustand'
+import { getSettings } from '@/api';
+import { SettingsStoreState } from '@/types/storeTypes';
+import { create } from 'zustand';
 
-export const useSettingsStore = create<SettingsStoreState>()(set => ({
+export const useSettingsStore = create<SettingsStoreState>()((set, get) => ({
   sociable: null,
   contacts: { ua: null, en: null },
   logo: null,
   competitions: { ua: null, en: null },
   fetchSettings: async language => {
     try {
-      const settings = await getSettings(language)
-      if (!settings) throw new Error('could not fetch the data from that resource')
-      const { contacts, logo, sociable, competitions } = settings[0]
-
-      set(state => {
-        return {
-          sociable,
-          logo,
-          contacts: { ...state.contacts, [language]: contacts },
-          competitions: { ...state.competitions, [language]: competitions },
-        }
-      })
+      const settings = await getSettings(language);
+      if (!settings)
+        throw new Error('could not fetch the data from that resource');
+      const { contacts, logo, sociable, competitions } = settings[0];
+      set({
+        sociable,
+        logo,
+        contacts: { ...get().contacts, [language]: contacts },
+        competitions: { ...get().competitions, [language]: competitions },
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   },
-}))
+  getPreviewSettings: (settings, language) => {
+    const { contacts, logo, sociable, competitions } = settings[1];
+    set({
+      sociable,
+      logo,
+      contacts: { ...get().contacts, [language]: contacts },
+      competitions: { ...get().competitions, [language]: competitions },
+    });
+  },
+}));
