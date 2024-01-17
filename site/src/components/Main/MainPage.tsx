a41a0a92ce67b283619844a112d7bf5211fdc1bd
Yevhen <onufrii.socnet@gmail.com>
2024-01-17 13:23:40 +0200

    added video to main section

--- site/src/components/Main/MainPage.tsx
+++ site/src/components/Main/MainPage.tsx
@@ -1,11 +1,47 @@
-import { Container, Box } from '@mui/material';
+import { AspectRatio, Box, Container } from '@mui/material'
+
+import bgVideo from '../../../public/bg_video.mp4'
 
 const MainPage = () => {
   return (
-    <Container>
-      <Box>main section</Box>
-    </Container>
-  );
-};
+    <Box
+      position={'relative'}
+      sx={{
+        width: '100%',
+        height: '100vh',
+        backgroundColor: 'rgba(8, 7, 8, 0.40)',
+        backdropFilter: 'blur(6px)',
+      }}
+    >
+      <Box
+        component={'video'}
+        src={bgVideo}
+        autoPlay
+        loop
+        muted
+        sx={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+        }}
+      />
+      <Box
+        sx={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          zIndex: 10,
+          bgcolor: 'rgba(0, 0, 0, 0.70);',
+        }}
+      ></Box>
+    </Box>
+  )
+}
 
-export default MainPage;
+export default MainPage
