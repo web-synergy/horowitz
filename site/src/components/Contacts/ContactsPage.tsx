9e0aca415e118725bfa72d2280730940b3a2e80d
Yevhen <onufrii.socnet@gmail.com>
2024-01-22 01:04:32 +0200

    fix styles on Contacts page

--- site/src/components/Contacts/ContactsPage.tsx
+++ site/src/components/Contacts/ContactsPage.tsx
@@ -3,12 +3,12 @@ import { useTranslation } from 'react-i18next'
 import { Routes } from '@/types/routes.d'
 import { FC } from 'react'
 
-import { Box, Container, Typography } from '@mui/material'
+import { Box, Container, Stack, Typography } from '@mui/material'
 import Breadcrumbs from '../Common/Breadcrumbs'
 import { Offset } from '../Common/Offset'
 import ContactsDetails from './parts/ContactsDetails'
 
-import { ContentWrapper, InfoDivider, Section } from './styled'
+import { ContentWrapper, InfoDivider, MainBox, Section } from './styled'
 
 import { useSettingsStore } from '@/store'
 import { PortableText, PortableTextComponents } from '@portabletext/react'
@@ -39,27 +39,38 @@ const ContactsPage: FC = () => {
       <Offset />
       <Container>
         <Breadcrumbs title={t(`navigation.${Routes.CONTACTS}`)} mode="dark" />
-        <ContentWrapper>
-          <Typography variant="h2" component={'h1'}>
+        <MainBox>
+          <Typography
+            variant="h2"
+            component={'h1'}
+            sx={{
+              marginBottom: {
+                xs: '24px',
+                md: '40px',
+              },
+            }}
+          >
             {t(`navigation.${Routes.CONTACTS}`)}
           </Typography>
-          <Box>
-            <Box sx={{ 'p:not(:last-child)': { marginBottom: '16px' } }}>
-              <PortableText value={about[0]} components={components} />
+          <ContentWrapper>
+            <Box>
+              <Box sx={{ '& :first-child': { marginBottom: 2 } }}>
+                <PortableText value={about[0]} components={components} />
+              </Box>
+              <InfoDivider variant="light" />
             </Box>
-            <InfoDivider variant="light" />
-          </Box>
-          <Box sx={{ width: '100%' }}>
-            <ContactsDetails {...{ location, phone, email }} />
-          </Box>
-          <Box sx={{ width: '100%' }}>
-            <ContactsDetails
-              pressCenterPhone={pressCenter.phone}
-              pressCenterEmail={pressCenter.email}
-            />
-          </Box>
-          <SocialMedia />
-        </ContentWrapper>
+            <Box sx={{ width: '100%' }}>
+              <ContactsDetails {...{ location, phone, email }} />
+            </Box>
+            <Box sx={{ width: '100%' }}>
+              <ContactsDetails
+                pressCenterPhone={pressCenter.phone}
+                pressCenterEmail={pressCenter.email}
+              />
+            </Box>
+            <SocialMedia />
+          </ContentWrapper>
+        </MainBox>
       </Container>
     </Section>
   )
