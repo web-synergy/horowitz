2207771454efa7e431456855788aa5b2bb9fc6d9
Andrii <andriy30534>
2024-01-22 18:08:40 +0200

    fix:build

--- site/src/store/newsStore.ts
+++ site/src/store/newsStore.ts
@@ -2,10 +2,10 @@ import { getNews } from '@/api';
 import { NewsStoreState } from '@/types/storeTypes';
 import { create } from 'zustand';
 
-export const useNewsStore = create<NewsStoreState>()((set, get) => ({
+export const useNewsStore = create<NewsStoreState>()(set => ({
   newsList: [],
   currentNews: null,
-  fetchNews: async (language, start, end) => {
+  fetchNews: async language => {
     try {
       const news = await getNews(language);
       if (!news) throw new Error('could not fetch the data from that resource');
