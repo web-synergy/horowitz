9c62fbacb1fded813235368a255d307996f72882
Andrii <andriy30534>
2024-01-22 12:56:23 +0200

    feat:added gallery component

--- site/src/components/News/prtableComponents/GridGallery.tsx
+++ site/src/components/News/prtableComponents/GridGallery.tsx
@@ -1,44 +1,32 @@
 import { urlFor } from '@/config/sanity/imageUrl';
 import { IPortableImgGallery } from '@/types/newsTypes';
-import { processImageUrl } from '@/utils/helpers';
-import { Box, Grow, ImageList, ImageListItem, Typography } from '@mui/material';
+import { Box, ImageList, ImageListItem, Typography } from '@mui/material';
 import { FC } from 'react';
 
-function srcset(image: string, size: number, rows = 1, cols = 1) {
-  const imageSrs = `${image}?w=${size * cols}&h=${
-    size * rows
-  }&fit=crop&auto=format`;
-  const imageSrsSet = `${image}?w=${size * cols}&h=${
-    size * rows
-  }&fit=crop&auto=format&dpr=2 2x`;
-  return {
-    src: processImageUrl(imageSrs),
-    srcSet: processImageUrl(imageSrsSet),
-  };
-}
+import GrowView from '@/components/Common/GrowView';
 
 export const GridGallery: FC<IPortableImgGallery> = ({ value }) => {
   const { images, title } = value;
 
   return (
-    <Box sx={{ mb: '24px' }}>
-      <Grow in={true} timeout={1200}>
-        <ImageList variant='quilted' cols={4} rowHeight={400}>
+    <GrowView>
+      <Box sx={{ mb: '24px' }}>
+        <ImageList variant='quilted' cols={4} rowHeight={200}>
           {images &&
-            images.map((item, index) => {
+            images.map(item => {
               if (item.asset)
                 return (
                   <ImageListItem
-                    key={index}
+                    sx={{ p: '3px' }}
+                    key={item._key}
                     cols={item.photoLayout.cols || 1}
                     rows={item.photoLayout.rows || 1}>
                     <img
-                      {...srcset(
-                        urlFor(item).crop('focalpoint').url(),
-                        400,
-                        item.photoLayout.rows,
-                        item.photoLayout.cols
-                      )}
+                      src={urlFor(item)
+                        .width(400)
+                        .height(200)
+                        .fit('fill')
+                        .url()}
                       alt={item.title}
                       loading='lazy'
                     />
@@ -46,8 +34,8 @@ export const GridGallery: FC<IPortableImgGallery> = ({ value }) => {
                 );
             })}
         </ImageList>
-      </Grow>
-      <Typography variant='smallText'>{title}</Typography>
-    </Box>
+        <Typography variant='smallText'>{title}</Typography>
+      </Box>
+    </GrowView>
   );
 };
--- site/src/components/News/prtableComponents/ImageComponent.tsx
+++ site/src/components/News/prtableComponents/ImageComponent.tsx
@@ -1,16 +1,39 @@
 import { urlFor } from '@/config/sanity/imageUrl';
-import { IImage } from '@/types/newsTypes';
-import { Box } from '@mui/material';
+import { IImage, IPortableImgGallery } from '@/types/newsTypes';
+import { Box, useMediaQuery } from '@mui/material';
+import { GridGallery } from './GridGallery';
+import { PortableSwiper } from './Swiper';
+import { theme } from '@/theme';
+import { FC } from 'react';
+import GrowView from '@/components/Common/GrowView';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const ImageComponent = ({ value }: { value: IImage }) => {
   return (
-    <Box
-      sx={{ width: '100%', mb: { xs: '32px', md: '40px' } }}
-      component={'img'}
-      src={urlFor(value).auto('format').fit('scale').url().toString()}
-      alt={value.alt || ' '}
-      loading='lazy'
-    />
+    <GrowView>
+      <Box sx={{ my: { xs: '32px', md: '40px' } }}>
+        <img
+          width={'100%'}
+          height={'auto'}
+          src={urlFor(value)
+            .width(920)
+            .height(520)
+            .fit('fill')
+            .url()
+            .toString()}
+          alt={value.alt || ' '}
+          loading='lazy'
+        />
+      </Box>
+    </GrowView>
   );
+};
+export const ImagesArray: FC<IPortableImgGallery> = ({ value }) => {
+  const { option } = value;
+  const isMob = useMediaQuery(theme.breakpoints.down('md'));
+  if (option && !isMob) {
+    return <GridGallery value={value} />;
+  } else {
+    return <PortableSwiper value={value} />;
+  }
 };
