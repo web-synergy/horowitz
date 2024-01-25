710edb77edd9e85796f3b67d31d941906f53cacd
Yevhen <onufrii.socnet@gmail.com>
2024-01-26 01:01:50 +0200

    styles correcting

--- site/src/components/Main/parts/CompetitionOrganizers/CompetitionOrganizers.tsx
+++ site/src/components/Main/parts/CompetitionOrganizers/CompetitionOrganizers.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Stack } from '@mui/material'
+import { Box, Container } from '@mui/material'
 import { FC } from 'react'
 
 import logo5 from '../../temp/organizers/academyGliyera_logo.png'
@@ -6,54 +6,33 @@ import logo3 from '../../temp/organizers/departmentСulture_logo.png'
 import logo4 from '../../temp/organizers/horowitz_logo.png'
 import logo1 from '../../temp/organizers/minCult_logo.png'
 import logo2 from '../../temp/organizers/stateAgency_logo.png'
+
 import { MainTitle } from '../WatchOnline/styled'
+import { LogotypesStack, Wrapper } from './styled'
 
 const CompetitionOrganizers: FC = () => {
+  // !temp
   const logotypes = [logo1, logo2, logo3, logo4, logo5]
 
   return (
-    <Box
-      component={'section'}
-      sx={{
-        marginBottom: {
-          xs: '72px',
-          md: '96px',
-          lg: '120px',
-        },
-      }}
-    >
+    <Wrapper component={'section'}>
       <Container>
         <MainTitle component={'h2'} sx={{ marginBottom: '48px', textAlign: 'center' }}>
           Організатори конкурсу
         </MainTitle>
-        <Stack
-          sx={{
-            flexDirection: 'row',
-            justifyContent: {
-              xs: 'center',
-              md: 'center',
-              lg: 'space-between',
-            },
-            alignItems: 'center',
-            flexWrap: 'wrap',
-            columnGap: '12px',
-            rowGap: '48px',
-          }}
-        >
+        <LogotypesStack>
           {logotypes.map((logo, i) => (
-            // <Box key={i} sx={{ height: '92px' }}>
             <Box
               key={i}
               component={'img'}
               src={logo}
               alt={'logo image'}
-              sx={{ maxWidth: '100%', maxHeight: '92px' }}
+              sx={{ maxWidth: '288px', maxHeight: '92px' }}
             />
-            // </Box>
           ))}
-        </Stack>
+        </LogotypesStack>
       </Container>
-    </Box>
+    </Wrapper>
   )
 }
 
