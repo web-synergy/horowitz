377b77f2ada32a056fe602302c5f586a99c4cdea
Yevhen <onufrii.socnet@gmail.com>
2024-01-27 01:43:54 +0200

    added slider to sponsors section

--- site/src/components/Main/parts/PartnersAndFriends/PartnersAndFriends.tsx
+++ site/src/components/Main/parts/PartnersAndFriends/PartnersAndFriends.tsx
@@ -1,13 +1,43 @@
-import { Box, Stack } from '@mui/material'
+import { Box, Stack, useMediaQuery, useTheme } from '@mui/material'
 import { FC } from 'react'
 import { MainTitle } from '../../styled'
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
+import './currSwiperStyles.css'
+
+import { Autoplay } from 'swiper/modules'
+import { sponsorsLogotypes } from './test'
+
 const PartnersAndFriends: FC = () => {
+  const { breakpoints } = useTheme()
+  const isTablet = useMediaQuery(breakpoints.down('lg'))
+  const isMobile = useMediaQuery(breakpoints.down('md'))
   return (
-    <Stack>
+    <Stack spacing={6} sx={{ marginBottom: '100px' }}>
       <MainTitle component={'h2'} sx={{ textAlign: 'center' }}>
         Партнери та друзі
       </MainTitle>
+      <Box sx={{ height: '150px' }}>
+        <Swiper
+          slidesPerView={isMobile ? 1.7 : 2.2}
+          centeredSlides={true}
+          spaceBetween={isTablet ? 40 : 150}
+          loop={true}
+          speed={2000}
+          autoplay={{
+            delay: 600,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+        >
+          {sponsorsLogotypes.map((logo, i) => (
+            <SwiperSlide id="sponsors-swiper-slide" key={i}>
+              <img src={logo} alt="logotype" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
     </Stack>
   )
 }
--- site/src/components/Main/parts/WatchOnline/styled.ts
+++ site/src/components/Main/parts/WatchOnline/styled.ts
@@ -12,24 +12,6 @@ export const Wrapper = styled(Box)<BoxProps>(({ theme }) => ({
   },
 }))
 
-// export const MainTitle = styled(Typography)<TypographyProps>(({ theme }) => ({
-//   [theme.breakpoints.up('xs')]: {
-//     fontSize: '1.75rem',
-//     lineHeight: 1.28,
-//     marginBottom: '24px',
-//   },
-//   [theme.breakpoints.up('md')]: {
-//     fontSize: '2.25rem',
-//     lineHeight: 1.22,
-//     marginBottom: '40px',
-//   },
-//   [theme.breakpoints.up('lg')]: {
-//     fontSize: '3rem',
-//     lineHeight: 1.16,
-//     marginBottom: '48px',
-//   },
-// }))
-
 export const StyledContainer = styled(Container)(({ theme }) => ({
   [theme.breakpoints.down('lg')]: {
     '&.MuiContainer-root': { paddingRight: 0 },
