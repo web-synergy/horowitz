a3a7c285d1ef8b02dee2f01040f187c37d10d50b
Andrii <andriy30534>
2024-01-27 10:23:43 +0200

    added typography styles

--- site/src/components/NewsCurrentPage/prtableComponents/YouTube.tsx
+++ site/src/components/NewsCurrentPage/prtableComponents/YouTube.tsx
@@ -14,15 +14,30 @@ export default function YouTube({
   const isMob = useMediaQuery(theme.breakpoints.down('md'));
   return (
     <GrowView>
-      <Box sx={{ mb: '24px' }}>
-        <ReactPlayer
-          style={{ width: '100%', height: '500px' }}
-          width={'100%'}
-          height={isMob ? 260 : 480}
-          controls
-          url={url}
-        />
-        <Typography variant='bodyRegular'>{title}</Typography>
+      <Box>
+        <Box sx={{ mb: '24px' }}>
+          <ReactPlayer
+            style={{ width: '100%', height: '500px' }}
+            width={'100%'}
+            height={isMob ? 260 : 480}
+            controls
+            url={url}
+          />
+        </Box>
+        <Typography
+          component={'p'}
+          sx={{
+            display: 'block',
+            fontSize: {
+              xs: '16px',
+              md: '20px',
+              lg: '24px',
+              color: theme => theme.palette.neutral[90],
+              my: { xs: '16px', md: '24px' },
+            },
+          }}>
+          {title}
+        </Typography>
       </Box>
     </GrowView>
   );
