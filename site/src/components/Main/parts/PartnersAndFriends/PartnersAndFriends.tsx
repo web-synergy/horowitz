d48562c08f35722057131e52b98b59d6f7c72a05
Yevhen <onufrii.socnet@gmail.com>
2024-01-28 17:22:16 +0200

    set up slider on sponsors section

--- site/src/components/Main/parts/PartnersAndFriends/PartnersAndFriends.tsx
+++ site/src/components/Main/parts/PartnersAndFriends/PartnersAndFriends.tsx
@@ -1,10 +1,9 @@
-import { Box, Stack, useMediaQuery, useTheme } from '@mui/material'
+import { Box, Stack } from '@mui/material'
 import { FC } from 'react'
 import { MainTitle } from '../../styled'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/swiper-bundle.css'
-import './currSwiperStyles.css'
 
 import { MainPage } from '@/types/translation.d'
 import { useTranslation } from 'react-i18next'
@@ -14,10 +13,6 @@ import { ShowMoreBtn } from './styled'
 import { sponsorsLogotypes } from './test'
 
 const PartnersAndFriends: FC = () => {
-  const { breakpoints } = useTheme()
-  const isTablet = useMediaQuery(breakpoints.down('lg'))
-  const isMobile = useMediaQuery(breakpoints.down('md'))
-
   const { t } = useTranslation()
   return (
     <Stack
@@ -35,9 +30,17 @@ const PartnersAndFriends: FC = () => {
       </MainTitle>
       <Box sx={{ height: '150px' }}>
         <Swiper
-          slidesPerView={isMobile ? 1.7 : 2.1}
           centeredSlides={true}
-          spaceBetween={isTablet ? 40 : 150}
+          breakpoints={{
+            1280: {
+              spaceBetween: 150,
+              slidesPerView: 2.2,
+            },
+            320: {
+              spaceBetween: 40,
+              slidesPerView: 1.7,
+            },
+          }}
           loop={true}
           speed={2000}
           autoplay={{
@@ -48,12 +51,20 @@ const PartnersAndFriends: FC = () => {
         >
           {sponsorsLogotypes.map((logo, i) => (
             <SwiperSlide id="sponsors-swiper-slide" key={i}>
-              <img src={logo} alt="logotype" />
+              <img
+                src={logo}
+                alt="logotype"
+                style={{
+                  maxWidth: '100%',
+                  maxHeight: '100%',
+                  objectFit: 'contain',
+                }}
+              />
             </SwiperSlide>
           ))}
         </Swiper>
       </Box>
-      <Box sx={{ textAlign: 'center' }}>
+      <Box sx={{ alignSelf: 'center' }}>
         <ShowMoreBtn variant="secondary" component={RouterLink} to={'/sponsors'}>
           {t(`mainPage.${MainPage.BTN_SHOW}`)}
         </ShowMoreBtn>
