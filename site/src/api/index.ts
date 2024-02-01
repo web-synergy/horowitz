dccb74478733b8681562737a1b3d2243b242cd3a
Yevhen <onufrii.socnet@gmail.com>
2024-02-01 20:16:05 +0200

    added function for fetching partners data

--- site/src/api/index.ts
+++ site/src/api/index.ts
@@ -1,33 +1,32 @@
-import { horowitzQuery } from './query';
-import { IHorowitzData } from '@/types/horowitzTypes';
+import { IHorowitzData } from '@/types/horowitzTypes'
+import { horowitzQuery } from './query'
 
-import { INews } from '@/types/newsTypes';
-import { sanityFetch } from '../config/sanity/client';
-import { SettingsResp } from '../types/contactsTypes';
-import { currentNewsQuery, newsQuery, settingsQuery } from './query';
+import { INews } from '@/types/newsTypes'
+import { sanityFetch } from '../config/sanity/client'
+import { SettingsResp } from '../types/contactsTypes'
+import { currentNewsQuery, partners, newsQuery, settingsQuery } from './query'
 
-export const getSettings = async (
-  language: string
-): Promise<SettingsResp[]> => {
-  return sanityFetch(settingsQuery, { language });
-};
+import { PartnersResp } from '@/types/partnersTypes'
 
-export const getHorowitzData = async (
-  language: string
-): Promise<IHorowitzData> => {
-  return sanityFetch(horowitzQuery, { language });
-};
+export const getSettings = async (language: string): Promise<SettingsResp[]> => {
+  return sanityFetch(settingsQuery, { language })
+}
+
+export const getHorowitzData = async (language: string): Promise<IHorowitzData> => {
+  return sanityFetch(horowitzQuery, { language })
+}
 
 export const getNews = async (
   language: string,
   firstEl: number,
   lastEl: number
 ): Promise<INews[]> => {
-  return sanityFetch(newsQuery, { language, firstEl, lastEl });
-};
-export const getCurrentNews = async (
-  language: string,
-  slug: string
-): Promise<INews> => {
-  return sanityFetch(currentNewsQuery, { language, slug });
-};
+  return sanityFetch(newsQuery, { language, firstEl, lastEl })
+}
+export const getCurrentNews = async (language: string, slug: string): Promise<INews> => {
+  return sanityFetch(currentNewsQuery, { language, slug })
+}
+
+export const getPartners = async (): Promise<PartnersResp> => {
+  return sanityFetch(partners)
+}
