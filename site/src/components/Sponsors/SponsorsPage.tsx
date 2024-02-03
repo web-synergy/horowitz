2237567cf6709941edc615ef63cdeb5be0ef6357
Yevhen <onufrii.socnet@gmail.com>
2024-02-03 17:56:52 +0200

    added butoon "go to main page"

--- site/src/components/Sponsors/SponsorsPage.tsx
+++ site/src/components/Sponsors/SponsorsPage.tsx
@@ -1,7 +1,9 @@
 import { usePartnersStore } from '@/store'
-import { Box, Container, Stack } from '@mui/material'
+import { Box, Container, Link, Stack } from '@mui/material'
 import { FC, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import PageTemplate from '../Common/PageTemplate'
+import SvgSpriteIcon from '../Common/SvgSpriteIcon'
 import LogotypesGallery from './parts/LogotypesGallery'
 import { MainTitle, TwoGalleryStack } from './styled'
 
@@ -15,7 +17,10 @@ const SponsorsPage: FC = () => {
   const mainInfoPartners = usePartnersStore(state => state.mainInfoPartners)
   const officialInfoPartners = usePartnersStore(state => state.officialInfoPartners)
 
+  const navigate = useNavigate()
+
   useEffect(() => {
+    scrollTo(0, 0)
     if (!organizers) fetchData()
   }, [])
 
@@ -57,6 +62,15 @@ const SponsorsPage: FC = () => {
               title="Офіційні інформаційні партнери"
               gallery={officialInfoPartners}
             />
+            <Link
+              sx={{
+                color: theme => theme.palette.neutral[60],
+              }}
+              onClick={() => navigate(-1)}
+            >
+              <SvgSpriteIcon sx={{ transform: 'rotate(90deg)' }} icon="arrow" />
+              Повернутись до головної сторінки
+            </Link>
           </Stack>
         </Container>
       </PageTemplate>
