5398c5f5b77c4ecc856f881707e38b56a2cf91ff
Yevhen <onufrii.socnet@gmail.com>
2024-02-05 02:50:18 +0200

    added bg image for Zheneva main page

--- site/src/components/KyivGeneva/KyivGenevaPage.tsx
+++ site/src/components/KyivGeneva/KyivGenevaPage.tsx
@@ -1,18 +1,79 @@
-import PageTemplate from '../Common/PageTemplate';
-import { Container, Grid, Box, Button } from '@mui/material';
-import { useTranslation } from 'react-i18next';
-import { kyivGenevaNavigation } from '@/config/routes/navigation';
-import { Link } from 'react-router-dom';
-import { Routes } from '@/types/routes.d';
+import { kyivGenevaNavigation } from '@/config/routes/navigation'
+import { Routes } from '@/types/routes.d'
+import { Box, Button, Container, Grid, Stack } from '@mui/material'
+import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
+import PageTemplate from '../Common/PageTemplate'
+
+import mainBgIng from '@/assets/images/kyiv-geneva/mainPage/zheneva_main_bg.jpg'
+import Breadcrumbs from '../Common/Breadcrumbs'
 
 const KyivGenevaPage = () => {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
+
   return (
     <PageTemplate>
+      <Stack sx={{ marginBottom: { xs: '72px', md: '90px', lg: '120px' } }}>
+        <Box
+          sx={{
+            position: 'relative',
+            paddingBottom: {
+              xs: 'calc(38.5% - 32px)',
+              md: 'calc(43.5% - 48px)',
+              lg: 'calc(42.5% - 48px)',
+            },
+            maxHeight: '544px',
+            overflow: 'hidden',
+            '::after': {
+              content: '""',
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              zIndex: 2,
+              bgcolor: '#080708B2',
+            },
+          }}
+        >
+          <Box
+            sx={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              zIndex: 1,
+            }}
+          >
+            <Box
+              component={'img'}
+              src={mainBgIng}
+              alt="background image"
+              sx={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+                objectPosition: 'bottom',
+              }}
+            />
+          </Box>
+          <Box sx={{ position: 'relative', zIndex: 5 }}>
+            <Container>
+              <Breadcrumbs mode="dark" title="Конкурс Горовиця Київ-Женева" />
+            </Container>
+          </Box>
+        </Box>
+        {/* <Box height={'120px'}></Box> */}
+      </Stack>
       <Container>
-        <div>KyivGenevaPage</div>
-        <Box>
-          <Grid container rowSpacing={6} columnSpacing={7}>
+        {/* <Box>
+          <Grid
+            container
+            rowSpacing={6}
+            columnSpacing={7}
+            justifyContent="space-between"
+          >
             {kyivGenevaNavigation.map((navigation) => (
               <Grid item key={navigation.title} lg={4} md={6} xs={12}>
                 <Button
@@ -42,10 +103,10 @@ const KyivGenevaPage = () => {
               WFIMC
             </Button>
           </Box>
-        </Box>
+        </Box> */}
       </Container>
     </PageTemplate>
-  );
-};
+  )
+}
 
-export default KyivGenevaPage;
+export default KyivGenevaPage
