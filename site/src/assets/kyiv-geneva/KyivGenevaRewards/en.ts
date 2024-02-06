9a069875ae04ad3b83cdaa4357af97bc6c140166
Andrii <andriy30534>
2024-02-06 14:56:01 +0200

    added data

---
+++ site/src/assets/kyiv-geneva/KyivGenevaRewards/en.ts
@@ -0,0 +1,62 @@
+export default [
+  {
+    h3: 'First prize',
+    p: 'Gold Medal and CHF 20.000',
+  },
+  {
+    h3: 'Second prize',
+    p: 'Silver Medal and CHF 15.000',
+  },
+  {
+    h3: 'Third prize',
+    p: 'Bronze Medal and CHF 10.000',
+  },
+  {
+    h3: 'Prizes, engagements',
+    listStrong: [
+      {
+        'VERE MUSIC FUND Prize ': `– Certificate and CHF 5.000
+Awarded to the best Ukrainian Artist`,
+      },
+      {
+        'Horowitz Prize ': `– Certificate and CHF 3.000
+For the best performance of a virtuoso Horowitz Transcription`,
+      },
+      {
+        'СWFIMC Prize ': `– Certificate and CHF 3.000
+Awarded for the best performance of a work written by a Ukrainian composer (regardless of the
+nationality of the participant)`,
+      },
+      {
+        'Audience Prize ': `- Certificate and Cash Award of CHF 3.000
+Donated by the European Broadcasting Union (EBU)`,
+      },
+      {
+        'Prix Fondation Thiébaud-Frey ': `– Certificate, performance at “La Prairie” in Bellmund, and Cash
+Award of CHF 3.000 Offered by the Thiébaud-Frey Foundation, Bellmund`,
+      },
+      {
+        'Chopin Prize ': `– Certificate, Cash Award of CHF 3.000
+Offered by the Fryderyk Chopin Society of Geneva
+Except for the winners of First, Second and Third prize, participants in the Semifinal (3rd
+Round) will receive an award of CHF 1.500.
+`,
+      },
+      {
+        "Winner's engagements ": `Steinway Prizewinner Concerts, offered by Steinway & Sons, Hamburg, Germany`,
+      },
+    ],
+    list: [
+      `South Korea Concert Tour offered by SBU Artists, Vienna, Austria`,
+      `Ferruccio Busoni International Piano Competition, Bolzano, Italy`,
+      `The Cliburn International Piano Competition, Fort Worth, USA`,
+      `Fryderyk Chopin International Piano Competition, Warsaw, Poland`,
+      `Franz Liszt International Piano Competition, Utrecht, The Netherlands`,
+      `ArtDIALOG Festival 2023, Biel/ Bienne, Switzerland`,
+      `All prizes will be awarded.`,
+      `Additional prizes and engagements will be announced during the coming months.`,
+      `Prize winners of the competition are obliged to comply with all contractual obligations of any
+offered concert engagements, which are an integral part of the competition ́s awards.`,
+    ],
+  },
+];
