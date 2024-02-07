7fa5ac2700dd0d881e49686d69761fda3cb6959a
Andrii <andriy30534>
2024-02-07 14:02:25 +0200

    fix styles

--- site/src/components/Common/LinkGoBack.tsx
+++ site/src/components/Common/LinkGoBack.tsx
@@ -1,17 +1,24 @@
-import { Link } from '@mui/material';
+import { Link, Stack } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 import SvgSpriteIcon from './SvgSpriteIcon';
 const LinkGoBack = ({ title, href }: { title: string; href: string }) => {
   return (
-    <Link
-      component={RouterLink}
-      to={href}
-      sx={{
-        color: theme => theme.palette.neutral[60],
-      }}>
-      <SvgSpriteIcon sx={{ transform: 'rotate(90deg)' }} icon='arrow' />
-      {title}
-    </Link>
+    <Stack>
+      <Link
+        component={RouterLink}
+        to={href}
+        sx={{
+          color: theme => theme.palette.neutral[60],
+          fontSize: { xs: '12px', md: '16px' },
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          flex: 1,
+        }}>
+        <SvgSpriteIcon sx={{ transform: 'rotate(90deg)' }} icon='arrow' />
+        {title}
+      </Link>
+    </Stack>
   );
 };
 export default LinkGoBack;
