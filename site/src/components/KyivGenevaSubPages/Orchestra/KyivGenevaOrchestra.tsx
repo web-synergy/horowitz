d704652aa15f96be01b40db6676eb983a40720fe
Andrii <andriy30534>
2024-02-07 14:02:55 +0200

    added new pages

--- site/src/components/KyivGenevaSubPages/Orchestra/KyivGenevaOrchestra.tsx
+++ site/src/components/KyivGenevaSubPages/Orchestra/KyivGenevaOrchestra.tsx
@@ -1,11 +1,84 @@
-import { Container, Typography } from '@mui/material';
+import { Box, Container, Typography, styled } from '@mui/material';
 import PageTemplate from '@/components/Common/PageTemplate';
+import { useTranslation } from 'react-i18next';
+import { Routes } from '@/types/routes.d';
+import { orchestraData } from '@/assets/kyiv-geneva/KyivGenevaOrchestra';
+import { theme } from '@/theme';
+import LinkGoBack from '@/components/Common/LinkGoBack';
+import { KyivGeneva } from '@/types/translation.d';
+import { useEffect } from 'react';
+
+const RichText = styled(Typography)(() => ({
+  display: 'block',
+  textAlign: 'justify',
+  marginBottom: '8px',
+  [theme.breakpoints.up('lg')]: {
+    marginBottom: '0px',
+  },
+}));
 
 const KyivGenevaOrchestra = () => {
+  const {
+    t,
+    i18n: { language },
+  } = useTranslation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0 });
+  }, []);
+  const data = orchestraData[language];
+
   return (
     <PageTemplate>
       <Container>
-        <Typography>Kyiv Geneva Orchestra</Typography>
+        <Box sx={{ my: { xs: '48px', lg: '120px' } }}>
+          <Typography
+            sx={{
+              mb: { xs: '24px', lg: '48px' },
+            }}
+            component={'h1'}
+            variant='h1'>
+            {t(`kyivGeneva.${Routes.KYIV_GENEVA_ORCHESTRA}`)}
+          </Typography>
+
+          {data.map((item, index) => (
+            <Box key={index}>
+              <Typography
+                sx={{ mb: { xs: '24px', lg: '48px' } }}
+                component={'h3'}
+                variant='h3'>
+                {item.title}
+              </Typography>
+              <Box
+                sx={{
+                  columnCount: { lg: 2 },
+                  columnGap: { lg: 3 },
+                  mb: { xs: '24px', lg: '48px' },
+                }}>
+                {item.text.map((item, index) => (
+                  <RichText key={index}>{item}</RichText>
+                ))}
+              </Box>
+              <Box sx={{ mb: { xs: '24px', lg: '48px' } }}>
+                <Box
+                  sx={{ width: '100%', maxHeight: '681px' }}
+                  component={'img'}
+                  src={item.img.src}
+                  alt={item.img.title}
+                />
+                <Typography
+                  sx={{ color: theme => theme.palette.neutral[60] }}
+                  variant='smallText'>
+                  {item.img.title}
+                </Typography>
+              </Box>
+            </Box>
+          ))}
+          <LinkGoBack
+            title={t(`kyivGeneva.${KyivGeneva.GO_KYIV_GENEVA}`)}
+            href={`/${Routes.KYIV_GENEVA}`}
+          />
+        </Box>
       </Container>
     </PageTemplate>
   );
--- site/src/components/KyivGenevaSubPages/Regulation/KyivGenevaRegulation.tsx
+++ site/src/components/KyivGenevaSubPages/Regulation/KyivGenevaRegulation.tsx
@@ -1,11 +1,65 @@
-import { Container, Typography } from '@mui/material';
+import { Box, Container, Stack, Typography, styled } from '@mui/material';
 import PageTemplate from '@/components/Common/PageTemplate';
+import { regulationData } from '@/assets/kyiv-geneva/KyivGenevaRegulation';
+import { useTranslation } from 'react-i18next';
+import { Routes } from '@/types/routes.d';
+import LinkGoBack from '@/components/Common/LinkGoBack';
+import { KyivGeneva } from '@/types/translation.d';
+import { useEffect } from 'react';
+
+const Title = styled(Typography)(() => ({
+  display: 'block',
+}));
+const Text = styled(Typography)(() => ({
+  display: 'block',
+  textAlign: 'justify',
+}));
 
 const KyivGenevaRegulation = () => {
+  const {
+    t,
+    i18n: { language },
+  } = useTranslation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0 });
+  }, []);
+  const data = regulationData[language];
   return (
     <PageTemplate>
       <Container>
-        <Typography>Kyiv Geneva Regulation</Typography>
+        <Box sx={{ my: { xs: '48px', lg: '120px' } }}>
+          <Typography component={'h1'} variant='h1'>
+            {t(`kyivGeneva.${Routes.KYIV_GENEVA_REQUIREMENTS}`)}
+          </Typography>
+
+          <Stack sx={{ my: '48px' }} direction={'column'} spacing={'48px'}>
+            {data.map(item => (
+              <Stack direction={'column'} spacing={'48px'}>
+                <Stack direction={'column'} spacing={'24px'}>
+                  <Title variant='h3'>{item.title}</Title>
+
+                  {item.rules.map(item => (
+                    <Text variant='bodyRegular'>{item}</Text>
+                  ))}
+                </Stack>
+                {item.date.map(item => (
+                  <Stack direction={'column'} spacing={'24px'}>
+                    <Text variant='bodyMedium'>{item.day}</Text>
+                    {item.description.map(item => (
+                      <Text variant='bodyRegular'>{item}</Text>
+                    ))}
+                  </Stack>
+                ))}
+              </Stack>
+            ))}
+          </Stack>
+          <Box sx={{ mt: { xs: '48px', lg: '62px' } }}>
+            <LinkGoBack
+              title={t(`kyivGeneva.${KyivGeneva.GO_KYIV_GENEVA}`)}
+              href={`/${Routes.KYIV_GENEVA}`}
+            />
+          </Box>
+        </Box>
       </Container>
     </PageTemplate>
   );
