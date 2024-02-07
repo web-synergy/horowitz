20dde0fe82573630745e002e44c732eb309d500c
Andrii <andriy30534>
2024-02-07 14:01:47 +0200

    added types to data

---
+++ site/src/assets/kyiv-geneva/KyivGenevaOrchestra.ts
@@ -0,0 +1,131 @@
+import orcestr from '../images/kyiv-geneva/orcestr.png';
+import conductor from '../images/kyiv-geneva/conductor.png';
+import { TOrchestra } from '@/types/kyivGenevaDataTypes';
+
+export const orchestraData: TOrchestra = {
+  ua: [
+    {
+      title: 'Оркестр',
+      img: {
+        src: orcestr,
+        title: '© Niels Ackermann',
+      },
+      text: [
+        `ORCHESTRE DE LA SUISSE ROMANDE
+(ОРКЕСТР РОМАНСЬКОЇ ШВЕЙЦАРІЇ)
+Заснований у 1918 році Ернестом Ансерме, який був його головним диригентом до 1967
+року, Оркестр романської Швейцарії (OSR) складається зі 112 музикантів. Колектив
+проводить абонементні концерти у Женеві та Лозанні, симфонічні програми для міста
+Женева, щорічний концерт до Дня ООН і супроводжує оперні вистави у Великому театрі
+Женеви.
+`,
+        `Із січня 2017 року посаду музичного та художнього керівника OSR обіймає британський
+диригент Джонатан Нотт. Він продовжує традиції діяльності засновника OSR і музичних
+керівників, які очолювали оркестр в різні роки: Пауля Клецького, Вольфганга Завалліша,
+Горста Штайна, Арміна Йордана, Фабіо Луїзі, Пінхаса Штейнберга, Марека Яновського,
+Нееме Ярві та головного запрошеного диригента Кадзукі Ямада. Наразі Деніел Хардінг
+займає позицію диригента в резиденції.
+`,
+      ],
+    },
+    {
+      title: 'Диригент',
+      text: [
+        `Кирило Карабиць – визнаний світом український диригент. Понад 14 років є головним
+диригентом Борнмутського симфонічного оркестру (Велика Британія). Під його орудою
+колектив постійно виступав на BBC Proms, у лондонському центрі мистецтв «Барбікан»,
+здійснив багато студійних записів, які отримали визнання критиків.
+Кирило Карабиць працював із багатьма провідними колективами Європи, Азії та Північної
+Америки, серед яких: симфонічні оркестри Клівленда, Філадельфії, Сан-Франциско та
+Чикаго, Національний оркестр Франції, Віденський симфонічний оркестр, оркестри
+Віденської, Мюнхенської та Роттердамської філармоній, Симфонічний оркестр Йоміурі
+Ніппон, Філармонічний оркестр Театру Ла Феніче та симфонічний оркестр BBC.
+`,
+        `Яскравими стали нещодавні виступи диригента з Даллаським, Піттсбурзьким,
+Балтіморським симфонічними оркестрами, Симфонічним оркестром Празького радіо тощо.
+Серед проведених та запланованих подій нинішнього сезону 2022-2023 років – постановки
+«Завмерлого міста» Е. Корнгольда в Англійській національній опері, «Богеми» Дж. Пуччіні
+у Цюрихському оперному театрі й «Так чинять усі» В. А. Моцарта на оперному фестивалі
+«Грейндж».`,
+        `Як оперний диригент Карабиць плідно співпрацював із Німецькою оперою, Цюрихським
+оперним театром, Штутгартським оперним театром, Гамбурзькою державною оперою,
+Англійською національною оперою, Глайндборнським оперним фестивалем, а також із
+Женевським фестивалем імені Р. Вагнера у рамках святкування ювілею композитора
+тощо.`,
+        `Як музичний керівник і головний диригент Німецького національного театру у
+Веймарі (у 2016-2019 роках) Кирило Карабиць диригував відомими постановками
+«Нюрнберзьких мейстерзінгерів» і «Тангойзера» Р. Вагнера, а також циклом опер В. А.
+Моцарта на лібрето Л. да Понте.`,
+        `Робота з молодими яскравими музикантами займає чільне місце у творчій діяльності
+Кирила Карабиця. Зокрема він був художнім керівником та диригентом молодіжного
+колективу «I, CULTURE Orchestra» у європейському турі. У 2012 та 2014 роках Кирило
+керував оркестром під час телевізійної трансляції фіналу премії «Молодий музикант року
+BBC». Серед останніх подій творчого життя музиканта – виступ із Національним
+молодіжним оркестром Великої Британії в турне країною.
+У 2013 році Королівське філармонічне товариство визнало Кирила Карабиця «Диригентом
+року».`,
+      ],
+      img: {
+        src: conductor,
+        title: '©︎ Ugo Ponte',
+      },
+    },
+  ],
+  en: [
+    {
+      title: 'Orchestra',
+      img: {
+        src: orcestr,
+        title: '© Niels Ackermann',
+      },
+      text: [
+        `ORCHESTRE DE LA SUISSE ROMANDE
+Founded in 1918 by Ernest Ansermet, who was its principal conductor until 1967, the Orchestre
+de la Suisse Romande (OSR) is made up of 112 permanent musicians. It gives subscription
+concerts in Geneva and Lausanne, symphonic programmes for the City of Geneva, the annual
+United Nations Day concert and accompanies operatic performances at the Grand Théâtre of
+Geneva. Over the decades, the OSR has built up an international repu-tation thanks to its
+historic recordings and its interpretation of the 20th-century repertories.`,
+        `The British conductor Jonathan Nott has served as the OSR’s Music and Artistic Director since
+January 2017, following in the steps of the OSR’s founding head and successive music
+directors: Paul Kletzki, Wolfgang Sawallisch, Horst Stein, Armin Jordan, Fabio Luisi, Pinchas
+Steinberg, Marek Janowski, Neeme Järvi, and its principal guest conductor Kazuki Yamada.
+Daniel Harding is currently Conductor in Residence.`,
+      ],
+    },
+    {
+      title: 'Conductor',
+      text: [
+        `Kirill Karabits has been Chief Conductor of the Bournemouth Symphony Orchestra for 14 years
+and their relationship has been celebrated worldwide. Together they have made many critically
+acclaimed recordings, performed regularly at the BBC Proms and appeared together at
+London’s Barbican Centre as part of the Beethoven celebrations in the 19-20 season.
+`,
+        `Karabits has worked with many of the leading ensembles of Europe, Asia and North America,
+including the Cleveland, Philadelphia, San Francisco and Chicago Symphony orchestras,
+Munich Philharmonic, Orchestre National de France, Philharmonia Orchestra, Wiener
+Symphoniker, Rotterdam Philharmonic, Yomiuri Nippon Symphony Orchestra, Orchestra
+Filarmonica del Teatro La Fenice and the BBC Symphony Orchestra.`,
+        `Recent highlights include Kirill’s debuts with the Dallas Symphony, Pittsburgh Symphony,
+Baltimore Symphony, and the Prague Radio Symphony, among others. Highlights of the 2022-
+23 season include Kirill’s return to the English National Opera for a production of “Die tote
+Stadt”, to Opernhaus Zürich for “La Boheme”, and to The Grange Festival for “Così fan tutte”.`,
+        `A prolific opera conductor, Karabits has worked with the Deutsche Oper, Opernhaus Zürich and
+Oper Stuttgart, Glyndebourne Festival Opera, Staatsoper Hamburg, English National Opera,
+Bolshoi Theatre and the Wagner Geneva Festival in celebration of the composer’s anniversary.
+Music Director of the Deutsches Nationaltheatre Weimar from 2016-19, Karabits conducted
+acclaimed productions of Wagner's Die Meistersinger von Nürnberg and Tannhäuser as well as
+Mozart's DaPonte Cycle.`,
+        `Working with the next generation of bright musicians is of great importance to Karabits and as
+Artistic Director of I, CULTURE Orchestra he conducted them on their European tour. In 2012
+and 2014 he conducted the televised finals of the BBC Young Musician of the Year Award), and
+has recently debuted with the National Youth Orchestra of Great Britain on a UK tour.
+Kirill was named Conductor of the Year at the 2013 Royal Philharmonic Society Music Awards.`,
+      ],
+      img: {
+        src: conductor,
+        title: '©︎ Ugo Ponte',
+      },
+    },
+  ],
+};
