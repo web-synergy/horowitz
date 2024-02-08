0199d5de78587e3ffec3b2df748ad2dacfb8101a
Andrii <andriy30534>
2024-02-08 10:24:58 +0200

    feat added data

---
+++ site/src/assets/kyiv-geneva/KyivGenevaParticipants.ts
@@ -0,0 +1,235 @@
+import David_Aladashvili from '@/assets/images/kyiv-geneva/participants/Aladashvili-David.jpeg';
+import Ana_Bakradze from '@/assets/images/kyiv-geneva/participants/Bakradze-Ana.jpeg';
+import Adam_Balogh from '@/assets/images/kyiv-geneva/participants/Balogh-Ádám.jpeg';
+import Elzbieta_Bilicka from '@/assets/images/kyiv-geneva/participants/Bilicka-Elzbieta.jpg';
+import Anfisa_Bobylova from '@/assets/images/kyiv-geneva/participants/Bobylova-Anfisa(1).jpg';
+import Anais_Cassiers from '@/assets/images/kyiv-geneva/participants/Cassiers-Anaïs.jpg';
+import Xuehong_Chen from '@/assets/images/kyiv-geneva/participants/Chen-Xuehong.jpg';
+import Boyuan_Cheng from '@/assets/images/kyiv-geneva/participants/Cheng-Boyuan.jpeg';
+import Jun_Hwi_Cho from '@/assets/images/kyiv-geneva/participants/Cho-Jun Hwi.jpg';
+import Roman_Fediurko from '@/assets/images/kyiv-geneva/participants/Fediurko-Roman.jpg';
+import Johanna_Goranko from '@/assets/images/kyiv-geneva/participants/Goranko-Joanna.jpg';
+import Giuseppe_Guarrera from '@/assets/images/kyiv-geneva/participants/Guarrera-Giuseppe.jpg';
+import Kyoshiro_Hirama from '@/assets/images/kyiv-geneva/participants/Hirama-Kyoshiro.jpg';
+import Carter_Johnson from '@/assets/images/kyiv-geneva/participants/Johnson-Carter.jpg';
+import Shota_Kaya from '@/assets/images/kyiv-geneva/participants/Kaya-Shota.jpeg';
+import Stanislav_Korchagin from '@/assets/images/kyiv-geneva/participants/Korchagin-Stanislav.jpg';
+import Kisuk_Kwon from '@/assets/images/kyiv-geneva/participants/Kwon-Kisuk.jpg';
+import Hyo_Lee from '@/assets/images/kyiv-geneva/participants/Lee-Hyo.jpg';
+import Hao_Wei_Lin from '@/assets/images/kyiv-geneva/participants/Lin-Hao- Wei.jpg';
+import Linnik_Denis from '@/assets/images/kyiv-geneva/participants/Linnik-Denis.jpg';
+import Roman_Lopatynskyi from '@/assets/images/kyiv-geneva/participants/Lopatynskyi-Roman.jpg';
+import Pedro_Lopez_Salas from '@/assets/images/kyiv-geneva/participants/López Salas-Pedro.jpeg';
+import Danilo_Mascetti from '@/assets/images/kyiv-geneva/participants/Mascetti-Danilo.jpg';
+import Kyoungsun_Park from '@/assets/images/kyiv-geneva/participants/Park-Kyoungsun.jpg';
+import Yuli_Qiao from '@/assets/images/kyiv-geneva/participants/Qiao-Yuli.jpeg';
+import Danylo_Saienko from '@/assets/images/kyiv-geneva/participants/Saienko-Danylo.jpg';
+import Wenting_Shi from '@/assets/images/kyiv-geneva/participants/Shi-Wenting.jpg';
+import Julian_Trevelyan from '@/assets/images/kyiv-geneva/participants/Trevelyan-Julian.jpg';
+import Yuanfan_Yang from '@/assets/images/kyiv-geneva/participants/Yang-Yuanfan-.jpg';
+
+export const participantsData = [
+  {
+    img: David_Aladashvili,
+    fullName: {
+      ua: 'Давид Аладашвілі',
+      en: 'David Aladashvili',
+    },
+  },
+  {
+    img: Ana_Bakradze,
+    fullName: {
+      ua: 'Ана Бакрадзе',
+      en: 'Ana Bakradze',
+    },
+  },
+  {
+    img: Adam_Balogh,
+    fullName: {
+      ua: 'Адам Балог',
+      en: 'Adam Balogh',
+    },
+  },
+  {
+    img: Elzbieta_Bilicka,
+    fullName: {
+      ua: 'Ельжбета Біліцька',
+      en: 'Elzbieta Bilicka',
+    },
+  },
+  {
+    img: Anfisa_Bobylova,
+    fullName: {
+      ua: 'Анфіса Бобильова',
+      en: 'Anfisa Bobylova',
+    },
+  },
+  {
+    img: Anais_Cassiers,
+    fullName: {
+      ua: 'Анаїс Касьєрс',
+      en: 'Anaïs Cassiers',
+    },
+  },
+  {
+    img: Xuehong_Chen,
+    fullName: {
+      ua: 'Хуехун Чень',
+      en: 'Xuehong Chen',
+    },
+  },
+  {
+    img: Boyuan_Cheng,
+    fullName: {
+      ua: 'Боюань Чен',
+      en: 'Boyuan Cheng',
+    },
+  },
+  {
+    img: Jun_Hwi_Cho,
+    fullName: {
+      ua: 'Чон Хві Чо',
+      en: 'Jun Hwi Cho',
+    },
+  },
+  {
+    img: Roman_Fediurko,
+    fullName: {
+      ua: 'Роман Федюрко',
+      en: 'Roman Fediurko',
+    },
+  },
+  {
+    img: Johanna_Goranko,
+    fullName: {
+      ua: 'Йоанна Горанко',
+      en: 'Johanna Goranko',
+    },
+  },
+  {
+    img: Giuseppe_Guarrera,
+    fullName: {
+      ua: 'Джузеппе Гуаррера',
+      en: 'Giuseppe Guarrera',
+    },
+  },
+  {
+    img: Kyoshiro_Hirama,
+    fullName: {
+      ua: 'Кіосіро Хірама',
+      en: 'Kyoshiro Hirama',
+    },
+  },
+  {
+    img: Carter_Johnson,
+    fullName: {
+      ua: 'Картер Джонсон',
+      en: 'Carter Johnson',
+    },
+  },
+  {
+    img: Shota_Kaya,
+    fullName: {
+      ua: 'Шота Кая',
+      en: 'Shota Kaya',
+    },
+  },
+  {
+    img: Stanislav_Korchagin,
+    fullName: {
+      ua: 'Станіслав Корчагін',
+      en: 'Stanislav Korchagin',
+    },
+  },
+  {
+    img: Kisuk_Kwon,
+    fullName: {
+      ua: 'Кісок Квон',
+      en: 'Kisuk Kwon',
+    },
+  },
+  {
+    img: Hyo_Lee,
+    fullName: {
+      ua: 'Хьо Лі',
+      en: 'Hyo Lee',
+    },
+  },
+  {
+    img: Hao_Wei_Lin,
+    fullName: {
+      ua: 'Хао Вей Лінь',
+      en: 'Hao Wei Lin',
+    },
+  },
+  {
+    img: Linnik_Denis,
+    fullName: {
+      ua: 'Денис Лінник',
+      en: 'Linnik Denis',
+    },
+  },
+  {
+    img: Roman_Lopatynskyi,
+    fullName: {
+      ua: 'Роман Лопатинський',
+      en: 'Roman Lopatynskyi',
+    },
+  },
+  {
+    img: Pedro_Lopez_Salas,
+    fullName: {
+      ua: 'Педро Лопес Салас',
+      en: 'Pedro López Salas',
+    },
+  },
+  {
+    img: Danilo_Mascetti,
+    fullName: {
+      ua: 'Данило Машетті',
+      en: 'Danilo Mascetti',
+    },
+  },
+  {
+    img: Kyoungsun_Park,
+    fullName: {
+      ua: 'Йонсон Пак',
+      en: 'Kyoungsun Park',
+    },
+  },
+  {
+    img: Yuli_Qiao,
+    fullName: {
+      ua: 'Юйлі Цяо',
+      en: 'Yuli Qiao',
+    },
+  },
+  {
+    img: Danylo_Saienko,
+    fullName: {
+      ua: 'Данило Саєнко',
+      en: 'Danylo Saienko',
+    },
+  },
+  {
+    img: Wenting_Shi,
+    fullName: {
+      ua: 'Веньтін Ши',
+      en: 'Wenting Shi',
+    },
+  },
+  {
+    img: Julian_Trevelyan,
+    fullName: {
+      ua: 'Джуліан Тревельян',
+      en: 'Julian Trevelyan',
+    },
+  },
+  {
+    img: Yuanfan_Yang,
+    fullName: {
+      ua: 'Юаньфань Ян',
+      en: 'Yuanfan Yang',
+    },
+  },
+];
--- site/src/components/KyivGenevaSubPages/Winners/KyivGenevaWinners.tsx
+++ site/src/components/KyivGenevaSubPages/Winners/KyivGenevaWinners.tsx
@@ -1,11 +1,66 @@
-import { Container, Typography } from '@mui/material';
+import { Box, Container, Grid, Typography } from '@mui/material';
 import PageTemplate from '@/components/Common/PageTemplate';
+import { Routes } from '@/types/routes.d';
+import { useTranslation } from 'react-i18next';
+import { winnersData } from '@/assets/kyiv-geneva/KyivGenevaWinners';
+import LinkGoBack from '../Common/LinkGoBack';
+import { Buttons } from '@/types/translation.d';
+import ImagePerson from '../Common/ImagePerson';
 
 const KyivGenevaWinners = () => {
+  const {
+    t,
+    i18n: { language },
+  } = useTranslation();
+
+  const data = {
+    mainImg: winnersData.mainImag,
+    winners: winnersData.winners[language],
+  };
   return (
     <PageTemplate>
-      <Container>
-        <Typography>Kyiv Geneva Winners</Typography>
+      <Container
+        sx={{
+          pt: { xs: 6, lg: 15 },
+          pb: { xs: 9, md: 12, lg: 15 },
+          display: 'flex',
+          flexDirection: 'column',
+          gap: { xs: '24px', lg: '48px' },
+        }}>
+        <Typography component={'h1'} variant='h1'>
+          {t(`navigation.${Routes.KYIV_GENEVA_WINNERS}`)}
+        </Typography>
+        <Grid container justifyContent={'center'} gap={'24px'}>
+          {data.winners.map(winner => (
+            <Grid xs={12} md={5.8} lg={3.8} item>
+              <ImagePerson alt={winner.fullName} img={winner.img} />
+              <Typography
+                variant='subhead'
+                component={'p'}
+                sx={{
+                  margin: '24px 0px 16px',
+                }}>
+                {winner.fullName}
+              </Typography>
+              <Typography
+                variant='bodyRegular'
+                component={'p'}
+                sx={{
+                  color: theme => theme.palette.action.focus,
+                  width: '100%',
+                }}>
+                {winner.prizePlace}
+              </Typography>
+            </Grid>
+          ))}
+        </Grid>
+        <Box width={'100%'} src={data.mainImg} component={'img'} />
+        <Box sx={{ mt: { xs: '24px', lg: 0 } }}>
+          <LinkGoBack
+            title={t(`buttons.${Buttons.GO_KYIV_GENEVA}`)}
+            href={`/${Routes.KYIV_GENEVA}`}
+          />
+        </Box>
       </Container>
     </PageTemplate>
   );
