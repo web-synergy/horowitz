dbc585242e5a66c5545ff31389bc237c296aeabf
Anna Prutnik <prutnik.anna@gmail.com>
2024-02-15 16:40:01 +0200

    fix some imports, remove breadcrumbs

--- site/src/components/KyivGenevaSubPages/WFIMC/KyivGenevaWFIMC.tsx
+++ site/src/components/KyivGenevaSubPages/WFIMC/KyivGenevaWFIMC.tsx
@@ -1,12 +1,11 @@
 import { useTranslation } from 'react-i18next';
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
 
 import PageTemplate from '@/components/Common/PageTemplate';
 import { Caption } from '@/components/KyivGeneva/parts/styled';
 
-import { Box, Container, Link, Stack, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import { FC, useEffect } from 'react';
-import { ImgBox, MainBox } from './syled';
+import { ImgBox, MainBox } from './styled';
 import GoBackBtn from '@/components/Common/GoBackBtn';
 import { Routes } from '@/types/routes.d';
 
@@ -15,10 +14,8 @@ import content from '@/assets/kyiv-geneva/main/geneva_mainPage.json';
 
 const KyivGenevaWFIMCPage: FC = () => {
   const {
-    t,
     i18n: { language },
   } = useTranslation();
-  const navigate = useNavigate();
 
   useEffect(() => {
     scrollTo(0, 0);
@@ -68,27 +65,6 @@ const KyivGenevaWFIMCPage: FC = () => {
               </Box>
             </Box>
             <Box sx={{ clear: 'both' }}></Box>
-
-            {/* <Stack
-              sx={{
-                marginTop: {
-                  xs: '24px',
-                  md: '48px',
-                },
-                flexDirection: {
-                  xs: 'column',
-                  md: 'row',
-                },
-                justifyContent: 'space-between',
-                gap: '48px',
-              }}
-            >
-              <Box sx={{ order: { xs: 1, md: 0 } }}>
-                <GoBackBtn title={t('genevaMainPage.BTN_GO_BACK')} onClick={() => navigate(-1)} />
-              </Box>
-
-             
-            </Stack> */}
           </MainBox>
         </Container>
         <GoBackBtn href={Routes.KYIV_GENEVA} />
