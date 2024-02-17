8e0438c88d16ee8225be53906fd59fb7629ef4d0
Yevhen <onufrii.socnet@gmail.com>
2024-02-17 17:52:51 +0200

    changed layout for tablet on WFIMC page

--- site/src/components/KyivGenevaSubPages/WFIMC/styled.ts
+++ site/src/components/KyivGenevaSubPages/WFIMC/styled.ts
@@ -1,22 +1,23 @@
-import { Box, styled } from '@mui/material';
+import { Box, styled } from '@mui/material'
 
 export const MainBox = styled(Box)(({ theme: { breakpoints } }) => ({
   paddingTop: '48px',
   paddingBottom: '72px',
 
   [breakpoints.up('md')]: {
+    paddingTop: '92px',
     paddingBottom: '96px',
   },
   [breakpoints.up('lg')]: {
-    padding: '120px 0',
+    padding: '92px 0 120px',
   },
-}));
+}))
 
 export const ImgBox = styled(Box)(({ theme }) => ({
   width: '100%',
-  [theme.breakpoints.up('lg')]: {
+  [theme.breakpoints.up('md')]: {
     float: 'right',
     width: 'calc(50% - 12px)',
     marginLeft: '24px',
   },
-}));
+}))
