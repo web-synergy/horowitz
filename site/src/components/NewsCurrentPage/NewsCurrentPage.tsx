df2e8b15e3ebbbc0a4ec3cf73b5a0a74a78bf040
Andrii <andriy30534>
2024-02-22 22:13:42 +0200

    fix margin

--- site/src/components/NewsCurrentPage/NewsCurrentPage.tsx
+++ site/src/components/NewsCurrentPage/NewsCurrentPage.tsx
@@ -46,11 +46,16 @@ const NewsCurrentPage = () => {
     <PageTemplate>
       <Container>
         {data && (
-          <Box sx={{ my: { xs: '40px', md: '48px', lg: '56px' } }}>
+          <Box
+            sx={{
+              mt: { xs: '24px', md: '48px' },
+              mb: { xs: '72px', md: '96px', lg: '120px' },
+            }}>
             <GrowView>
               <Box
                 sx={{
                   width: '100%',
+                  height: 'auto',
                 }}
                 src={
                   data.img?.asset &&
@@ -68,7 +73,7 @@ const NewsCurrentPage = () => {
             <Box sx={{ maxWidth: '930px', mx: 'auto' }}>
               <Typography
                 sx={{
-                  mt: '54px',
+                  mt: { xs: '24px', md: '48px' },
                   mb: '24px',
                   display: 'block',
                   color: theme => theme.palette.neutral[50],
