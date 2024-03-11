36e4806e513a3cd869d9e8e5ccc82291832dca61
Anna Prutnik <prutnik.anna@gmail.com>
2024-03-11 17:13:25 +0200

    remove settings from vite config

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -11,19 +11,19 @@ export default defineConfig({
       '#root': resolve(__dirname, '..'),
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id
-              .toString()
-              .split('node_modules/')[1]
-              .split('/')[0]
-              .toString();
-          }
-        },
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes('node_modules')) {
+  //           return id
+  //             .toString()
+  //             .split('node_modules/')[1]
+  //             .split('/')[0]
+  //             .toString();
+  //         }
+  //       },
+  //     },
+  //   },
+  // },
 });
