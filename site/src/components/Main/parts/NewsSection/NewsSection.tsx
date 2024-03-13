6ec1310ab489cffa575c3ae8fae78febc1bdc492
Andrii <andriy30534>
2024-03-13 19:56:45 +0200

    remove tempo

--- site/src/components/Main/parts/NewsSection/NewsSection.tsx
+++ site/src/components/Main/parts/NewsSection/NewsSection.tsx
@@ -6,36 +6,22 @@ import { Routes } from '@/types/routes.d';
 
 import { FC } from 'react';
 
-import { Box, Button, Container } from '@mui/material';
+import { Container } from '@mui/material';
 
 import { NewsBox } from './styled';
 
 import { useHomeStore } from '@/store/homeStore';
 
 import NewsSwiper from '@/components/NewsSection/NewsSwiper';
-import { Link } from 'react-router-dom';
 
 const NewsSection: FC = () => {
-  const news = useHomeStore((state) => state.news);
+  const news = useHomeStore(state => state.news);
   const { t } = useTranslation();
 
   return (
     <NewsBox component={'section'}>
       <>
         <Container>
-          <Box sx={{ display: 'flex', gap: 1 }}>
-            <Button component={Link} to={'pdf/sql'}>
-              SQL
-            </Button>
-            <Button component={Link} to={'pdf/Книжка'}>
-              Книжка
-            </Button>
-
-            <Button component={Link} to={'pdf/Буклет'}>
-              Буклет
-            </Button>
-          </Box>
-
           {news && (
             <NewsSwiper
               title={t(`navigation.${Routes.NEWS}`)}
