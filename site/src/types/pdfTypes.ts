ca863d12ea35af4aa1ce75874a17bdaaf1219e8b
Andrii <andriy30534>
2024-03-13 19:06:23 +0200

    added pdf type

---
+++ site/src/types/pdfTypes.ts
@@ -0,0 +1,15 @@
+export interface PdfSize {
+  width: number;
+  height: number;
+}
+
+export interface IPdfViewer {
+  pdfSize: PdfSize;
+  pageNumber: number[];
+  isOnePage: boolean;
+}
+
+export interface IFileResponse {
+  title: string;
+  URL: string;
+}
