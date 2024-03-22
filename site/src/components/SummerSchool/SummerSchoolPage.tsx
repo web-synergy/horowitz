68b259f37f14960aaa2badb247ecbf11c6b0fad3
Yevhen <onufrii.socnet@gmail.com>
2024-03-22 21:36:15 +0200

    add main title

--- site/src/components/SummerSchool/SummerSchoolPage.tsx
+++ site/src/components/SummerSchool/SummerSchoolPage.tsx
@@ -1,7 +1,24 @@
-import InDevelopmentPage from '../InDevelopmentPage/InDevelopmentPage';
+import { SummerSchool } from '@/types/translation.d'
+import { Container, Typography } from '@mui/material'
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import PageTemplate from '../Common/PageTemplate'
+import { CommonStackWrapper } from '../Common/styledComponents'
 
-const SummerSchoolPage = () => {
-  return <InDevelopmentPage />;
-};
+const SummerSchoolPage: FC = () => {
+  const { t } = useTranslation()
 
-export default SummerSchoolPage;
+  return (
+    <PageTemplate>
+      <Container>
+        <CommonStackWrapper>
+          <Typography component={'h1'} variant="h1" textAlign={'center'}>
+            {t(`summerSchool.${SummerSchool.TITLE}`)}
+          </Typography>
+        </CommonStackWrapper>
+      </Container>
+    </PageTemplate>
+  )
+}
+
+export default SummerSchoolPage
