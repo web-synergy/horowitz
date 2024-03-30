d35122669c0f41719999741bd120359b93809410
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2024-03-30 16:53:21 +0200

    feat: add inputs for shedules

--- site/src/components/SummerSchoolSubPages/Schedules/Schedules.tsx
+++ site/src/components/SummerSchoolSubPages/Schedules/Schedules.tsx
@@ -1,5 +1,115 @@
-const Schedules = () => {
-  return <div>Schedules for Summer School</div>;
+import React, { useState } from "react";
+import {
+  TextField,
+  Button,
+  MenuItem,
+  Stack,
+  Container,
+  Box,
+  InputLabel,
+} from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
+import { useAnnualSummerSchoolStore } from "@/store/annualSummerSchoolStore";
+import Loader from "@/components/Common/Loader";
+import { useTranslation } from "react-i18next";
+import dayjs from "dayjs";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+
+const SchedulePage = () => {
+  const [professor, setProfessor] = useState("");
+  const [selectedDate, setSelectedDate] = useState(null);
+  const { t } = useTranslation();
+
+  // const { professors, isLoading, requestLang } = useAnnualSummerSchoolStore(
+  //   (state) => ({
+  //     professors: state.professors,
+  //     isLoading: state.isLoading,
+  //     requestLang: state.requestLang,
+  //   })
+  // );
+
+  // console.log(professors);
+
+  // if (isLoading) return <Loader />;
+  // if (!requestLang.length) return null;
+
+  const handleProfessorChange = (event) => {
+    setProfessor(event.target.value);
+  };
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  const handleShowSchedule = () => {
+    // Ваша логика по отображению графика
+  };
+
+  return (
+    <Container sx={{ marginTop: "40px" }}>
+      <Box
+        sx={{
+          padding: "16px 36px",
+          backgroundColor: "rgba(176, 115, 15, 0.1);",
+          marginTop: "40px",
+          marginBottom: "40px",
+          display: "flex",
+          alignItems: "flex-end",
+          gap: "30px",
+          justifyContent: "space-between",
+          // "& > *": { flexGrow: 1 },
+        }}
+      >
+        <Box>
+          <InputLabel sx={{ color: "black" }} htmlFor="professor-select">
+            Выберите профессора
+          </InputLabel>
+          <TextField
+            sx={{ width: "328px" }}
+            id="professor-select"
+            select
+            value={professor}
+            onChange={handleProfessorChange}
+          >
+            {/* <MenuItem disabled value="">
+            <em>Выберите профессора</em>
+          </MenuItem> */}
+            {/* Пример элементов меню */}
+            <MenuItem value="Профессор 1">Профессор 1</MenuItem>
+            <MenuItem value="Профессор 2">Профессор 2</MenuItem>
+            <MenuItem value="Профессор 3">Профессор 3</MenuItem>
+            {/* {professors.map((professor) => (
+            <MenuItem key={professor.id} value={professor.name}>
+              {professor.name}
+            </MenuItem>
+          ))} */}
+          </TextField>
+        </Box>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer sx={{ padding: 0 }} components={["DatePicker"]}>
+            <DemoItem label="Дата репетиції">
+              <DatePicker
+                sx={{ width: "328px", padding: 0 }}
+                // defaultValue={dayjs("2022-04-17")}
+              />
+            </DemoItem>
+          </DemoContainer>
+        </LocalizationProvider>
+        <Button
+          sx={{ width: "328px" }}
+          variant="contained"
+          color="primary"
+          onClick={handleShowSchedule}
+        >
+          Показать график
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
-export default Schedules;
+export default SchedulePage;
--- site/src/theme/components.ts
+++ site/src/theme/components.ts
@@ -1,25 +1,32 @@
-import { Components } from '@mui/material'
-import { MuiAccordion, MuiAccordionSummary } from './themed-components/accordion.modifier'
-import { MuiAppBar } from './themed-components/appbar.modifier'
-import { MuiBreadcrumbs } from './themed-components/breadcrumbs.modifier'
-import { MuiButton } from './themed-components/button.modifier'
-import { MuiContainer } from './themed-components/container.modifier'
-import { MuiCssBaseline } from './themed-components/cssBaseline.modifier'
-import { MuiDivider } from './themed-components/divider.modifier'
-import { MuiIconButton } from './themed-components/iconButton.modifier'
-import { MuiLink } from './themed-components/link.modifier'
-import { MuiList, MuiListItem, MuiListItemButton } from './themed-components/list.modifier'
-import { MuiSvgIcon } from './themed-components/svgIcon.modifier'
+import { Components } from "@mui/material";
+import {
+  MuiAccordion,
+  MuiAccordionSummary,
+} from "./themed-components/accordion.modifier";
+import { MuiAppBar } from "./themed-components/appbar.modifier";
+import { MuiBreadcrumbs } from "./themed-components/breadcrumbs.modifier";
+import { MuiButton } from "./themed-components/button.modifier";
+import { MuiContainer } from "./themed-components/container.modifier";
+import { MuiCssBaseline } from "./themed-components/cssBaseline.modifier";
+import { MuiDivider } from "./themed-components/divider.modifier";
+import { MuiIconButton } from "./themed-components/iconButton.modifier";
+import { MuiLink } from "./themed-components/link.modifier";
+import {
+  MuiList,
+  MuiListItem,
+  MuiListItemButton,
+} from "./themed-components/list.modifier";
+import { MuiSvgIcon } from "./themed-components/svgIcon.modifier";
 import {
   MuiInputBase,
   MuiOutlinedInput,
   MuiTextField,
-} from './themed-components/textField.modifier'
-import { MuiTypography } from './themed-components/typography.modifier'
+} from "./themed-components/textField.modifier";
+import { MuiTypography } from "./themed-components/typography.modifier";
 
-import { MuiFab } from './themed-components/fab.modifier'
+import { MuiFab } from "./themed-components/fab.modifier";
 
-import { Theme } from '@mui/material/styles'
+import { Theme } from "@mui/material/styles";
 
 export const components: Components<Theme> = {
   MuiContainer,
@@ -32,13 +39,13 @@ export const components: Components<Theme> = {
   MuiBreadcrumbs,
   MuiAppBar,
   MuiDivider,
-  MuiInputBase,
-  MuiOutlinedInput,
-  MuiTextField,
+  // MuiInputBase,
+  // MuiOutlinedInput,
+  // MuiTextField,
   MuiList,
   MuiListItem,
   MuiListItemButton,
   MuiAccordion,
   MuiAccordionSummary,
   MuiFab,
-}
+};
