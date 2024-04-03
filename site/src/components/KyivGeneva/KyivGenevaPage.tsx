780ce81d006a5a0e15b2a19604c51f58193f5608
Yevhen <onufrii.socnet@gmail.com>
2024-04-04 01:16:27 +0300

    added CommonStackWrapper for KyivGenevaPage

--- site/src/components/KyivGeneva/KyivGenevaPage.tsx
+++ site/src/components/KyivGeneva/KyivGenevaPage.tsx
@@ -1,29 +1,29 @@
-import { useTranslation } from 'react-i18next';
-import { MainStack } from './styled';
-import { Box } from '@mui/material';
+import { useTranslation } from 'react-i18next'
+import { Box } from '@mui/material'
 
-import AboutSection from './parts/AboutSection';
-import ButtonsSection from '../Templates/ButtonsSection';
-import SponsorsSection from './parts/SponsorsSection';
-import WFIMCSection from './parts/WFIMCSection';
-import { kyivGenevaNavigation } from '@/config/routes/navigation';
-import pianoImg from '@/assets/images/kyiv-geneva/mainPage/geneva_bg_piano.webp';
-import WFIMC_members from '@/assets/images/kyiv-geneva/wfimc/wfimc.webp';
-import pageData from '@/assets/kyiv-geneva/main/geneva_mainPage.json';
-import PageTemplate from '../Common/PageTemplate';
+import AboutSection from './parts/AboutSection'
+import ButtonsSection from '../Templates/ButtonsSection'
+import SponsorsSection from './parts/SponsorsSection'
+import WFIMCSection from './parts/WFIMCSection'
+import { kyivGenevaNavigation } from '@/config/routes/navigation'
+import pianoImg from '@/assets/images/kyiv-geneva/mainPage/geneva_bg_piano.webp'
+import WFIMC_members from '@/assets/images/kyiv-geneva/wfimc/wfimc.webp'
+import pageData from '@/assets/kyiv-geneva/main/geneva_mainPage.json'
+import PageTemplate from '../Common/PageTemplate'
+import { CommonStackWrapper } from '../Common/styledComponents'
 
 const KyivGenevaPage = () => {
   const {
     i18n: { language },
-  } = useTranslation();
+  } = useTranslation()
 
   if (!(language === 'ua' || language === 'en')) {
-    return <Box />;
+    return <Box />
   }
 
   return (
     <PageTemplate>
-      <MainStack>
+      <CommonStackWrapper>
         <AboutSection content={pageData[language]} />
         <ButtonsSection
           bgImage={pianoImg}
@@ -40,14 +40,11 @@ const KyivGenevaPage = () => {
             },
           }}
         />
-        <WFIMCSection
-          image={WFIMC_members}
-          wfimc_content={pageData[language].wfimc}
-        />
+        <WFIMCSection image={WFIMC_members} wfimc_content={pageData[language].wfimc} />
         <SponsorsSection />
-      </MainStack>
+      </CommonStackWrapper>
     </PageTemplate>
-  );
-};
+  )
+}
 
-export default KyivGenevaPage;
+export default KyivGenevaPage
