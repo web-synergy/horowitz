3c04b2ccc3fa66edc8709b2128b3b88a35e3cbd6
Anna Prutnik <prutnik.anna@gmail.com>
2024-07-10 17:46:41 +0300

    disabled exit button from search bar

--- site/src/components/Header/parts/SearchBar.tsx
+++ site/src/components/Header/parts/SearchBar.tsx
@@ -3,11 +3,14 @@ import {
   Container,
   IconButton,
   Stack,
+  Box,
   useTheme,
   useMediaQuery,
 } from '@mui/material';
+import { useLocation } from 'react-router-dom';
 import SvgSpriteIcon from '@/components/Common/SvgSpriteIcon';
 import SearchInput from './SearchInput';
+import { Routes } from '@/types/routes.d';
 
 interface SearchBarProps {
   onCloseSearchBar: () => void;
@@ -16,12 +19,23 @@ interface SearchBarProps {
 const SearchBar: FC<SearchBarProps> = ({ onCloseSearchBar }) => {
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up('lg'));
+  const { pathname } = useLocation();
+
+  const isSearchPage = pathname === `/${Routes.SEARCH}`;
   return (
     <Container>
       <Stack direction={'row'} alignItems={'center'} gap={'60px'}>
-        <SearchInput type="bar" />
-        {isDesktop && (
-          <IconButton onClick={onCloseSearchBar} sx={{ padding: 0 }}>
+        <Box sx={{ width: isDesktop ? '92.5%' : '100%' }}>
+          <SearchInput type="bar" />
+        </Box>
+
+        {isDesktop && !isSearchPage && (
+          <IconButton
+            onClick={onCloseSearchBar}
+            sx={{
+              padding: 0,
+            }}
+          >
             <SvgSpriteIcon icon="close" />
           </IconButton>
         )}
