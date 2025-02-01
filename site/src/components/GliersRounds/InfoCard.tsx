1643462c10a134863958a6a0bccbb498701686f0
Anna Prutnik <prutnik.anna@gmail.com>
2025-02-01 19:58:38 +0200

    add modal with info

---
+++ site/src/components/GliersRounds/InfoCard.tsx
@@ -0,0 +1,83 @@
+import { Box, Typography, useTheme, IconButton } from '@mui/material';
+import SvgSpriteIcon from '../Common/SvgSpriteIcon';
+
+interface InfoCardProps {
+  name: string;
+  years: string;
+  data: string;
+  group: number;
+  onClose: () => void;
+}
+
+export const InfoCard = ({
+  data,
+  name,
+  years,
+  group,
+  onClose,
+}: InfoCardProps) => {
+  const theme = useTheme();
+  const color =
+    group === 1 ? theme.palette.primary.main : theme.palette.secondary.main;
+  return (
+    <Box
+      sx={{
+        position: 'absolute',
+        width: { xs: '90%', md: '60%', lg: '40%' },
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        aspectRatio: 1,
+        height: 'auto',
+        borderRadius: { xs: '10%', md: '50%' },
+        backgroundColor: color,
+        padding: '5%',
+        display: 'flex',
+        justifyItems: 'center',
+        alignItems: 'center',
+        color: (theme) => theme.palette.common.white,
+      }}
+    >
+      <IconButton
+        sx={{
+          position: 'absolute',
+          color: color,
+          top: { xs: '-7%', md: 0 },
+          right: { xs: 0, md: '5%' },
+          cursor: 'pointer',
+        }}
+        aria-label="close button"
+        onClick={onClose}
+      >
+        <SvgSpriteIcon icon="close" size="large" />
+      </IconButton>
+      <Box>
+        <Typography
+          sx={{
+            textAlign: 'center',
+          }}
+          variant="h3"
+        >
+          {name}
+        </Typography>
+        <Typography
+          sx={{
+            marginBottom: { xs: 3, md: 5, lg: 6 },
+            textAlign: 'center',
+          }}
+          variant={'h3'}
+          component={'p'}
+        >
+          {years}
+        </Typography>
+        <Typography
+          variant="bodyMedium"
+          sx={{ textAlign: 'center' }}
+          component="p"
+        >
+          {data}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
--- site/src/components/GliersRounds/Member.tsx
+++ site/src/components/GliersRounds/Member.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import { RoundMemberData } from '@/libs/mockedData';
 import { CirclesType } from '@/utils/arrangeCircles';
-import Image from '../Common/Image';
-import { Box } from '@mui/material';
-import person from './person.png';
+
+import { Modal } from '@mui/material';
+import { BigCard } from './BigCard';
+import { SmallCard } from './SmallCard';
+import { InfoCard } from './InfoCard';
 
 interface MemberProps extends RoundMemberData, CirclesType {}
 
@@ -18,132 +21,55 @@ export const Member = ({
   shiftY,
   shiftX,
   image,
+  data,
 }: MemberProps) => {
   const ratio = l === 1 ? 1.08 : 1;
   const itemShiftY = shiftY || 1;
   const itemShiftX = shiftX || 1;
+  const [isOpen, setIsOpen] = useState(false);
 
-  if (isBig) {
-    return (
-      <Box
-        sx={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          width: d * 1.5,
-          height: d * 1.5,
-          borderRadius: '50%',
-
-          transform: `translate(${x * ratio - (d * itemShiftX) / 2}px, ${
-            y * ratio - (d * itemShiftY) / 2
-          }px)`,
-        }}
-      >
-        <Box
-          component="svg"
-          viewBox="0 0 100 100"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          sx={{
-            color: (theme) =>
-              group === 1
-                ? theme.palette.primary.main
-                : theme.palette.secondary.main,
-          }}
-        >
-          <path
-            id="circlePath"
-            fill="none"
-            d="M 10, 50 a 40,40 0 1,1 80,0 40,40 0 1,1 -80,0"
-          />
-          <text width="100">
-            <textPath href="#circlePath" fontSize={'70%'} fontWeight="bold">
-              {name + ' ' + years}
-            </textPath>
-          </text>
-          <path
-            d="M 15, 50 a 35,35 0 1,1 70,0 35,35 0 1,1 -70,0"
-            fill="currentColor"
-          />
-        </Box>
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            width: d * 1.05,
-            height: d * 1.05,
-            transform: 'translate(-50%, -50%)',
-            backgroundColor: '#efefef',
-            borderRadius: '50%',
-            overflow: 'clip',
-          }}
-        >
-          <Image
-            width={d * 1.05}
-            height={d * 1.05}
-            src={image || person}
-            isLazyLoading={false}
-            alt={name}
-            styles={{ objectFit: 'cover', objectPosition: 'center' }}
-          />
-        </Box>
-      </Box>
-    );
-  }
+  const onOpenModal = () => setIsOpen(true);
+  const onCloseModal = () => setIsOpen(false);
 
   return (
-    <Box
-      sx={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        width: d * 1.1,
-        height: d * 1.1,
-        borderRadius: '50%',
-
-        transform: `translate(${x * ratio - (d * itemShiftX) / 2}px, ${
-          y * ratio - (d * itemShiftY) / 2
-        }px)`,
-      }}
-    >
-      <Box
-        component="svg"
-        viewBox="0 0 100 100"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        sx={{
-          color: (theme) =>
-            group === 1
-              ? theme.palette.primary.main
-              : theme.palette.secondary.main,
-        }}
-      >
-        <path
-          id="circlePath"
-          fill="none"
-          d="M 10, 50 a 40,40 0 1,1 80,0 40,40 0 1,1 -80,0"
+    <>
+      {isBig ? (
+        <BigCard
+          x={x}
+          d={d}
+          y={y}
+          itemShiftX={itemShiftX}
+          itemShiftY={itemShiftY}
+          ratio={ratio}
+          group={group}
+          name={name}
+          years={years}
+          image={image}
+          onClick={onOpenModal}
         />
-        <text width="100">
-          <textPath href="#circlePath" fontSize={'70%'} fontWeight="bold">
-            {name}
-          </textPath>
-        </text>
-        <path
-          d="M 15, 50 a 35,35 0 1,1 70,0 35,35 0 1,1 -70,0"
-          fill="currentColor"
+      ) : (
+        <SmallCard
+          x={x}
+          d={d}
+          y={y}
+          itemShiftX={itemShiftX}
+          itemShiftY={itemShiftY}
+          ratio={ratio}
+          group={group}
+          name={name}
+          years={years}
+          onClick={onOpenModal}
         />
-        <text
-          x="50%"
-          y="50%"
-          textAnchor="middle"
-          fontSize={'55%'}
-          fontWeight="bold"
-          fill={'#fff'}
-        >
-          {years}
-        </text>
-      </Box>
-    </Box>
+      )}
+      <Modal open={isOpen} onClose={onCloseModal}>
+        <InfoCard
+          data={data}
+          name={name}
+          years={years}
+          group={group}
+          onClose={onCloseModal}
+        />
+      </Modal>
+    </>
   );
 };
